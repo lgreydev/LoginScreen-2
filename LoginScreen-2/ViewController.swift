cc357fe6961804fd2ac08d0ff33a9f87576771d8
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-05 13:21:34 +0300

    Initial Commit

---
+++ LoginScreen-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreen-2
+//
+//  Created by Sergey Lukaschuk on 05.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
