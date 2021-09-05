6287c9c75f840c73eb151c0b3637dac900b66158
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-05 19:07:01 +0300

    Add apple button

--- LoginScreen-2/ViewController.swift
+++ LoginScreen-2/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var signGoogleButton: UIView!
+    @IBOutlet var signAppleButton: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        signAppleButton.layer.cornerRadius = 15
+        signAppleButton.layer.borderWidth = 1
+        signAppleButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        
+        signGoogleButton.layer.cornerRadius = 15
+        signGoogleButton.layer.borderWidth = 1
+        signGoogleButton.layer.borderColor = #colorLiteral(red: 0, green: 0.5275259614, blue: 0.9681959748, alpha: 1)
     }
 
 
