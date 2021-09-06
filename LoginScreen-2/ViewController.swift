96dcaa7161e9a849563d214754951d9fa560172d
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-06 16:23:51 +0300

    Add log in button

--- LoginScreen-2/ViewController.swift
+++ LoginScreen-2/ViewController.swift
@@ -9,24 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    @IBOutlet var buttonViews: [UIView]!
     
-    @IBOutlet var signGoogleButton: UIView!
-    @IBOutlet var signAppleButton: UIView!
-    
-
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        signAppleButton.layer.cornerRadius = 15
-        signAppleButton.layer.borderWidth = 1
-        signAppleButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        
-        signGoogleButton.layer.cornerRadius = 15
-        signGoogleButton.layer.borderWidth = 1
-        signGoogleButton.layer.borderColor = #colorLiteral(red: 0, green: 0.5275259614, blue: 0.9681959748, alpha: 1)
+        for view in buttonViews {
+            switch view.restorationIdentifier {
+            case "login": loginButtonConfigure(view)
+            case "apple": signAppleButtonConfigure(view)
+            case "google": signGoogleButtonConfigure(view)
+            default: break
+            }
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func loginButtonConfigure(_ view: UIView) {
+        view.layer.cornerRadius = 15
+    }
+    
+    private func signAppleButtonConfigure(_ view: UIView) {
+        view.layer.cornerRadius = 15
+        view.layer.borderWidth = 1
+        view.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     }
 
-
+    private func signGoogleButtonConfigure(_ view: UIView) {
+        view.layer.cornerRadius = 15
+        view.layer.borderWidth = 1
+        view.layer.borderColor = #colorLiteral(red: 0, green: 0.5275259614, blue: 0.9681959748, alpha: 1)
+    }
 }
 
